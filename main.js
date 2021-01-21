cf7f47f3fe5285b72f6b9f60c938e76dd5020dd2
Davi Mury <56702643+davimury@users.noreply.github.com>
2021-01-21 17:40:05 -0300

    bootstrapstudio save

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
 
-  mainWindow.webContents.openDevTools()
-  mainWindow.loadFile('view/index.html')
-  mainWindow.removeMenu()
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadFile('view/index.html')
+    mainWindow.removeMenu()
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow()
+
+    app.on('activate', function() {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
--- preload.js
+++ preload.js
@@ -2,23 +2,12 @@ const binance = require('./controller/binance-interface.js')
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  function getCoinPrice(coin) {
-    binance.prices(coin, (error, ticker) => {
-      replaceText(coin, Object.values(ticker)[0])
-    })
-  }
+    binance.websockets.prevDay('BTCBRL', (error, response) => {
+        replaceText()
+    });
 
-  const replaceText = (selector, text) => {
-    let element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  getCoinPrice('BTCBRL')
-  getCoinPrice('BTCUSDT')
-
-  const reloadButton = document.getElementById('reload')
-  reloadButton.addEventListener("click", () => {
-    getCoinPrice('BTCBRL')
-    getCoinPrice('BTCUSDT')
-  })
+    const replaceText = (text) => {
+        let element = document.getElementsByClassName('.BTCBRL .price')
+        if (element) element.innerText = 'text'
+    }
 })
